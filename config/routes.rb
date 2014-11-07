bc9347a9ca1a76e8ee1999e80777b7ccd27c0273
kangkyu <kangkyu1111@gmail.com>
2014-11-06 16:21:41 -0800

    generate controller and edit views of pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 MyEtsydemo::Application.routes.draw do
+  get "pages/about"
+  get "pages/contact"
+
+  root 'pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
