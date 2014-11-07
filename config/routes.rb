314deaf5d01eed8ef019f50ede3aaad61ac282d4
kangkyu <kangkyu1111@gmail.com>
2014-11-06 17:04:45 -0800

    did migration (add schema file) and change root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ MyEtsydemo::Application.routes.draw do
   get "pages/about"
   get "pages/contact"
 
-  root 'pages#about'
+  root 'listings#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
