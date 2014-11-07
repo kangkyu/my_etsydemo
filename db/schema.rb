8ff4836731f07a29853d82b4ef10a34806580aec
kangkyu <kangkyu1111@gmail.com>
2014-11-07 12:56:53 -0800

    add image column to listings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141107002540) do
+ActiveRecord::Schema.define(version: 20141107205131) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141107002540) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
